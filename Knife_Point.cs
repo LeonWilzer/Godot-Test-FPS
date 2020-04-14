22583a37f05110b990623fbd58cf2e583700a552
Leon Wilzer <leon.wilzer@protonmail.com>
2020-04-14 12:45:34 +0000

    Inserted licenese in forgotten files

--- Knife_Point.cs
+++ Knife_Point.cs
@@ -2,15 +2,13 @@
 This file is part of Godot-Test-FPS
 
 Godot-Test-FPS is the source code for a game.
-Copyright (C) 2020 Leon Wilzer
+Copyright (C) 2020 Leon Wilzer <leon.wilzer@protonmail.com>
 
 Godot-Test-FPS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
 
 Godot-Test-FPS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
-
-Contact: leon.wilzer@protonmail.com
 */
 
 using Godot;
@@ -29,11 +27,13 @@ public class Knife_Point : WeaponPoint
         isWeaponEnabled = false;
     }
 
-    public override void FireWeapon(){
+    public override void FireWeapon()
+    {
         Area Area = GetNode<Area>("Area");
         var Bodies = Area.GetOverlappingBodies();
 
-        foreach (PhysicsBody Body in Bodies){
+        foreach (PhysicsBody Body in Bodies)
+        {
             if (Body == playernode)
                 return;
             HitTest.BulletHit(Body, damage, Area.GlobalTransform);
--- Pistol_Point.cs
+++ Pistol_Point.cs
@@ -1,3 +1,16 @@
+/*
+This file is part of Godot-Test-FPS
+
+Godot-Test-FPS is the source code for a game.
+Copyright (C) 2020 Leon Wilzer <leon.wilzer@protonmail.com>
+
+Godot-Test-FPS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+Godot-Test-FPS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 using Godot;
 using System;
 
@@ -16,7 +29,8 @@ public class Pistol_Point : WeaponPoint
         _bulletscene = ResourceLoader.Load<PackedScene>("Bullet_Scene.tscn");
     }
 
-    public override void FireWeapon(){
+    public override void FireWeapon()
+    {
         var clone = (Bullet_Scene)_bulletscene.Instance();
         Node sceneRoot = GetTree().Root.GetChild(0);
         sceneRoot.AddChild(clone);
