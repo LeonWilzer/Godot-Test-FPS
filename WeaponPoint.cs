bced08f5c51cc025439f723cb34e8f45c16f311c
Leon Wilzer <leon.wilzer@protonmail.com>
2020-04-14 12:19:42 +0000

    Added license boilerplate to all relevant files

--- WeaponPoint.cs
+++ WeaponPoint.cs
@@ -1,3 +1,18 @@
+/*
+This file is part of Godot-Test-FPS
+
+Godot-Test-FPS is the source code for a game.
+Copyright (C) 2020 Leon Wilzer
+
+Godot-Test-FPS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+Godot-Test-FPS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: leon.wilzer@protonmail.com
+*/
+
 using Godot;
 using System;
 
